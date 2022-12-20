0848915ce10a674d0954dbd0e2a6ed36a05a2510
horatiucd <horatiucd@gmail.com>
2022-12-20 12:34:32 +0200

    Adds shipping component.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
+  items: Product[] = [];
+
+  constructor(private cartService: CartService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.items = this.cartService.getCartItems();
+  }
 }
